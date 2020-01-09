35eace9505854b60df68b96da4a6a2c2b05ba60b
cassandralin <cassandralin92@gmail.com>
2020-01-09 20:54:40 +0000

    added return statement if array is empty

--- head.js
+++ head.js
@@ -7,7 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 function head(array){
-return array[0];
+  if(array.length === 0){
+    return undefined;
+  } 
+  return array[0];
 };
 
 assertEqual(head([5,6,7]), 5);
