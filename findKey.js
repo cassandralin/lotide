796bddc83ef9b95946e61ccbc0b58a49176afa2a
cassandralin <cassandralin92@gmail.com>
2020-01-09 20:31:12 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(object, callback){
+var names = Object.keys(object)
+  for (let key of names){
+    if (callback(object[key])){
+      return key;
+    } 
+  }
+  return undefined;
+};
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
