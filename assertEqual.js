629096033d75ebd12332d58df8985e72252ed94a
cassandralin <cassandralin92@gmail.com>
2020-01-13 14:47:15 +0000

    removed tests from head.js && assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🛑🛑🛑Assertion Passed: " + actual + "===" + expected);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("✅✅✅Assertation Failed: " + actual + "!==" + expected);
+    console.log(`🛑🛑🛑Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function head(array){
   if(array.length === 0){
@@ -13,5 +7,4 @@ function head(array){
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
