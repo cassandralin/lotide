4bff14afc2d27294ad4d4959b9c51eca6e7be65c
cassandralin <cassandralin92@gmail.com>
2020-01-13 18:19:48 +0000

    added tailTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,11 +1,6 @@
 const assertEqual = require('../assertEqual');
-const head = require('../head');
-const tail = require('../tail');
+
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail(["Lighthouse", "Labs"]), "Labs");
